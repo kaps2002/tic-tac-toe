ab212bff58a9df7271e6a2e80cb5d5a2be259e64
Karan Pandey <karpandey2002@gmail.com>
2022-07-13 17:00:16 +0530

    Adde board state and events

--- src/components/board.js
+++ src/components/board.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Square from './square';
 const Board =()=> {
+    const [board,setBoard]=useState(Array(9).fill(null));
+    const [isxnext,setisxnext]=useState(false);
+    const handlesquareclick=position=>{
+        if(board[position]){
+            return;
+        }
+        setBoard(prev=>{
+            return prev.map((square,pos)=>{
+                if(pos === position){
+                    return isxnext ? 'X':'O' ;
+                }
+                return square;
+            });
+        });
+        setisxnext((prev)=> !prev);
+    };
+    const rendersquare= position =>{
+        return (
+            <Square 
+                value={board[position]} 
+                onClick={() => handlesquareclick(position)}
+            />
+        );
+    };
     return(
         <div className='board'>
             <div className='board-row'>
-                <Square value={0} />
-                <Square value={1} />
-                <Square value={2} />
+                {rendersquare(0)}
+                {rendersquare(1)}
+                {rendersquare(2)}  
             </div>
             <div className='board-row'>
-                <Square value={3}/>
-                <Square value={4}/>
-                <Square value={5}/>
+                {rendersquare(3)}
+                {rendersquare(4)}
+                {rendersquare(5)}
             </div>
             <div className='board-row'>
-                <Square value={6}/>
-                <Square value={7}/>
-                <Square value={8}/>
+                {rendersquare(6)}
+                {rendersquare(7)}
+                {rendersquare(8)}
             </div>
         </div>
     )
