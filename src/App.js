49662116d7f9dbe24cfab34cb089d2852737a490
Karan Pandey <karpandey2002@gmail.com>
2022-07-15 11:48:01 +0530

    Added game history

--- src/App.js
+++ src/App.js
@@ -1,35 +1,47 @@
 import React,{useState} from 'react';
 import './App.css';
 import Board from './components/board';
+import History from './components/history';
+
 import './styles/root.scss';
 import {calculatewinner} from './helpers'
 const App =()=> {
+  const [history,sethistory] =useState([{board:Array(9).fill(null),isxnext: true}]);
+  const [currentmove,setcurrentmove]=useState(0);
+  const current =history[currentmove];
   const [board,setBoard]=useState(Array(9).fill(null));
-    const [isxnext,setisxnext]=useState(false);
+    
+  const [isxnext,setisxnext]=useState(false);
     const winner = calculatewinner(board);
     console.log(winner);
-    const message=winner ? `Winner is ${winner}`:`Next Player is ${isxnext ? 'X':'O'}`;
+    const message=winner ? `Winner is ${winner}`:`Next Player is ${current.isxnext ? 'X':'O'}`;
     const handlesquareclick=position=>{
-        if(board[position] || winner){
+        if(current.board[position] || winner){
             return;
         }
-        setBoard(prev=>{
-            return prev.map((square,pos)=>{
+        sethistory(prev=>{
+          const last=prev[prev.length-1];
+            const newboard= last.board.map((square,pos)=>{
                 if(pos === position){
-                    return isxnext ? 'X':'O' ;
+                    return last.isxnext ? 'X':'O' ;
                 }
                 return square;
             });
+            return prev.concat({board:newboard, isxnext: !last.isxnext});
+
         });
-        setisxnext((prev)=> !prev);
+        setcurrentmove(prev=> prev+1);
     };
+    const moveTo=(move)=>{
+      setcurrentmove(move);
+    }
   return (
     <>
       <div className='app'>
           <h1>TIC-TAC-TOE</h1>
           <h2>{message}</h2>
-          <Board board={board} handlesquareclick={handlesquareclick}/>
-
+          <Board board={current.board} handlesquareclick={handlesquareclick}/>
+          <History history={history} moveTo={moveTo} currentmove={currentmove}/>
       </div>
     </>
   );
